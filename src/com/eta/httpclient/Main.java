6076dd072045c7178eb5ebfe2eeb16716bcc6391
eta-development-team <eta.development.team@outlook.com>
2015-02-19 22:25:39 +0300

    Add real uri

--- src/com/eta/httpclient/Main.java
+++ src/com/eta/httpclient/Main.java
@@ -49,15 +49,14 @@ public class Main {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         Date startDateTime = dateFormat.parse("2015-01-01");
-        Date endDateTime = dateFormat.parse("2015-01-31");
+        Date endDateTime = dateFormat.parse("2015-01-09");
 
-        // https://eta.asd116.ru
         String baseAddress = "https://eta.asd116.ru:8433";
 
         String uri;
 
         uri = String.format(
-                "%s//api/package/archive?" +
+                "%s/api/package/archive?" +
                 "ar.withDictionaries=true&ar.startDateTime=%s&" +
                 "ar.endDateTime=%s&responseToFile=true",
                 baseAddress,
@@ -79,19 +78,24 @@ public class Main {
 
             connection.setRequestMethod("GET");
             connection.setRequestProperty("login", "Archive.Downloader");
-            connection.setRequestProperty("password", "xxxxx");
+            connection.setRequestProperty("password", "");
 
             try(InputStream xml = connection.getInputStream())
             {
                if(connection.getResponseCode() == 200)
                {
-                   Path path = Paths.get("d:\\ArchivePackageJv.xml");
+                   String desktop = System.getProperty("user.home") + "\\Desktop\\";
+                   Path path = Paths.get(desktop + "ArchivePackageJv.xml");
 
                    if (Files.exists(path))
                    {
                        Files.delete(path);
                    }
                    Files.copy(xml, path);
+                   System.out.println
+                   (
+                        String.format("Чтение архива выполнено. Результаты записаны в файл: %s", desktop)
+                   );
                }
             }
         }
